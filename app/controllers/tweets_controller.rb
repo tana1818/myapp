29ed4523b0101ab80892e88c038da3e52f8677cd
tana1818 <tanakagen1124@gmail.com>
2019-01-31 16:26:54 +0900

    ツイート画面作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,10 @@ class TweetsController < ApplicationController
       @tweets = Tweet.all
     end
 
+    def new
+    end
+
+    def create
+      Tweet.create(text: params[:text])
+    end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'tweets' => 'tweets#index' #ツイート一覧画面
-  
+  get 'tweets/new' => 'tweets#new' #ツイート投稿画面
+  post 'tweets' => 'tweets#create' #ツイート投稿機能
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
