81e7fa95c62af8a4d78e58d1080de09a85cd4036
tana1818 <tanakagen1124@gmail.com>
2019-01-31 17:21:32 +0900

    投稿機能とページネーション

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,18 @@
 class TweetsController < ApplicationController
 
     def index
-      @tweets = Tweet.all
+      @tweets = Tweet.order("created_at DESC").page(params[:page]).per(4)
     end
 
     def new
     end
 
     def create
-      Tweet.create(text: params[:text])
+      Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+      params.permit(:text)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'tweets#index'
   get 'tweets' => 'tweets#index' #ツイート一覧画面
   get 'tweets/new' => 'tweets#new' #ツイート投稿画面
   post 'tweets' => 'tweets#create' #ツイート投稿機能
