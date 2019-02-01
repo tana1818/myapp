d32d6b61087aec2f1b8fa848d34ca7c2d75d1883
tana1818 <tanakagen1124@gmail.com>
2019-02-01 16:41:49 +0900

    最低の機能は実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,39 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
     def index
-      @tweets = Tweet.order("created_at DESC").page(params[:page]).per(4)
+      @tweets = Tweet.order("created_at DESC").page(params[:page]).per(7)
     end
 
     def new
     end
 
     def create
-      Tweet.create(tweet_params)
+      Tweet.create(text: tweet_params[:text], user_id: current_user.id)
+    end
+
+    def destroy
+      tweet = Tweet.find(params[:id])
+      tweet.destroy if tweet.user_id == current_user.id
+    end
+
+    def edit
+      @tweet = Tweet.find(params[:id])
+    end
+
+    def update
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.update(tweet_params)
+      end
     end
 
     private
     def tweet_params
       params.permit(:text)
     end
-  end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
+end
